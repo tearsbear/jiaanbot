a0c87f41bfe729e0101dfa78a2b39716071b7f5c
tearsbear <jiehanandika@gmail.com>
2020-12-23 23:24:57 +0700

    try to fix scheduler

--- main.py
+++ main.py
@@ -603,6 +603,10 @@ def reminder():
     bot = telegram.Bot(token=TOKEN)
     bot.send_message(idj, 'halo jiaan')
 
+def test():
+    bot = telegram.Bot(token=TOKEN)
+    bot.send_message(idj, 'halo j')    
+
 def error_callback(update, context):
   try:
       raise context.error
@@ -658,18 +662,18 @@ def main():
 
   dispatcher.add_error_handler(error_callback)
 
+  # Create the job in schedule.
+  schedule.every().minute.at(":01").do(reminder)
+
+  Thread(target=schedule_checker).start() 
+  test()
+
  # Start the Bot
   updater.start_webhook(listen="0.0.0.0",
                         port=int(PORT),
                         url_path=TOKEN)
   updater.bot.setWebhook('https://jiaanbot.herokuapp.com/' + TOKEN)
   updater.idle()
-
-  # Create the job in schedule.
-  schedule.every().minute.at(":50").do(reminder)
-
-  Thread(target=schedule_checker).start() 
-#   reminder_birthday()
 
 if __name__ == '__main__':
     main()
